3e768fb3e8825448a00aeea18839857ac8bb638b
njegos-dukic <njegos.dukic.998@gmail.com>
2022-01-28 09:24:40 +0100

    Initial commit

---
+++ dms-admin-api/src/main/java/org/unibl/etf/sni/dms/DmsAdminApiApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.sni.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmsAdminApiApplication.class, args);
+    }
+
+}
